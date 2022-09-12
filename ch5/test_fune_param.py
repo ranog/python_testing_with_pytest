a311ce5884a1e5c34a9f039d0cd78450f56ba1f9
ranog <jprnogueira@yahoo.com.br>
2022-09-11 21:56:24 -0300

    Parametrizing Functions

---
+++ ch5/test_fune_param.py
@@ -0,0 +1,18 @@
+import pytest
+from cards import Card
+
+
+@pytest.mark.parametrize(
+    'start_summary, start_state',
+    [
+        ('write a book', 'done'),
+        ('second edition', 'in prog'),
+        ('create a course', 'todo'),
+    ],
+)
+def test_finish(cards_db, start_summary, start_state):
+    initial_card = Card(summary=start_summary, state=start_state)
+    index = cards_db.add_card(initial_card)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == 'done'
